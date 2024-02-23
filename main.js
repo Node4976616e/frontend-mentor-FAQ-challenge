b944d2f779f16a645a029deff312f6a6b83a4909
Ivan Poch <45315570+Node4976616e@users.noreply.github.com>
2024-02-23 16:38:03 +0100

    added functionality

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-const question_1 = document.querySelector("#question-1");
-const question_2 = document.querySelector("#question-2");
-const question_3 = document.querySelector("#question-3");
-const question_4 = document.querySelector("#question-4");
-let img = 1;
-addEventListener("click", function () {
-  let i = document.querySelector("#answer-1");
-  let b = document.querySelector("#question-1>img");
+var question_item = document.getElementsByClassName("question-item");
+var i;
 
-  i.classList.toggle("hidden");
-  if (img == 1) {
-    b.src = "images/icon-minus.svg";
-    img = 0;
-  } else if (img == 0) {
-    b.src = "images/icon-plus.svg";
-    img = 1;
-  }
-});
+for (i = 0; i < question_item.length; i++) {
+  question_item[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("hidden");
+    let img = this.lastElementChild; // select last element child (img tag)
+    /* Toggle between hiding and showing the active panel */
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+      img.src = "images/icon-plus.svg";
+    } else {
+      answer.style.display = "block";
+      img.src = "images/icon-minus.svg";
+    }
+  });
+}
